ab0ad9fc1a342e7e86a7f3772e3ead21b4880379
anshul bansal <akb.tech17@gmail.com>
2020-10-01 19:54:06 +0530

    Updates

---
+++ precache-manifest.772b3ac3a5de680dd8d49152fe2f27d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84e79f1f2663630d6640dea60ebd3e5",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "972627c53d6372e0d9cf",
+    "url": "/todo-list/static/css/main.92bd934a.chunk.css"
+  },
+  {
+    "revision": "2cc03c440bf286072e10",
+    "url": "/todo-list/static/js/2.c068cca6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.c068cca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972627c53d6372e0d9cf",
+    "url": "/todo-list/static/js/main.0cf52b4a.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
